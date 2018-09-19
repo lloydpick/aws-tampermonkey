a7f605d874d99ea6095b87bc5be9582fbeae72ef
Lloyd Pick <lloydpick@gmail.com>
2018-09-19 12:24:22 -0400

    don’t hardcode exclusions, use settings

--- inspector/cis-rule-exclusions.user.js
+++ inspector/cis-rule-exclusions.user.js
@@ -3,38 +3,42 @@
 // @homepageURL  https://github.com/lloydpick/aws-tampermonkey
 // @downloadURL  https://github.com/lloydpick/aws-tampermonkey/raw/master/inspector/cis-rule-exclusions.user.js
 // @supportURL   https://github.com/lloydpick/aws-tampermonkey/issues
-// @version      0.2
+// @version      0.3
 // @description  Fades out issues you know about
 // @author       Lloyd Pick <lloydpick@gmail.com>
 // @match        https://*.console.aws.amazon.com/inspector/home*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-var cisBenchmarkExclusions = [
-    "1.1.2",
-    "1.1.6",
-    "1.1.7",
-    "1.1.11",
-    "1.1.12",
-    "1.1.13",
-    "2.2.7",
-    "3.3.3",
-    "3.4.3",
-    "3.6.2",
-    "3.6.3",
-    "3.6.5",
-    "4.2.1.4"
-]
-
 function lookForExclusions(node) {
-    if (node.text().match(/ ((\d+\.)(\d+\.)?(\d+))/g)) {
-        var ruleId = node.text().match(/ ((\d+\.)(\d+\.)?(\d+))/)
-        if (cisBenchmarkExclusions.includes(ruleId[1])) {
+    if (node.text().match(/\s[0-9]*(([0-9]+\.)*[0-9]+)/g)) {
+        var ruleId = node.text().match(/\s[0-9]*(([0-9]+\.)*[0-9]+)/)
+        if (exclusionsArr.includes(ruleId[1])) {
             node.css("text-decoration", "line-through");
             node.closest(".ui-grid-cells").css("opacity", "0.25");
         }
     }
 }
 
+GM_config.init({
+  'id': 'cisExclusions',
+  'title': 'CIS Exclusion Settings',
+  'fields': {
+    'Exclusions': {
+      'label': 'Rule Exclusions (comma separated)',
+      'type': 'textarea',
+      'default': ''
+    }
+  }
+});
+
+GM_registerMenuCommand("Set CIS Exclusions", () => {
+    GM_config.open();
+});
+
+var exclusionsRaw = GM_config.get('Exclusions');
+var exclusionsArr = exclusionsRaw.split(",").map(item => item.trim());
 waitForKeyElements(".finding .ui-grid .ui-grid-body .ui-grid-row .col4 .ui-grid-cell-contents", lookForExclusions);
